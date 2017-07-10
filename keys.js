abd18ccb42f81a7db847070321c89427ed86fdeb
Matt <ms4224@hotmail.com>
2017-07-10 18:34:27 -0500

    uploading files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'NhbDHLudL9JL5c9apWT2pzm3o',
+  consumer_secret: 'FnvzmsyggHViWZ2JVmSp0dZnT3XlDxbyMhu8oVKFRq4F7rZkKC',
+  access_token_key: ' 2928010868-0Zc1QIYkJS513DwxsVw6M5oq0dsUoI0NilAnzKQ',
+  access_token_secret: 'att9VGItkGIONRMF2qpxmvsOAXLKFVc35S7miM6L8BTCP',
+}
